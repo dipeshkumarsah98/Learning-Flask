cdef260eac87ef59f32c4559a3cc9fbaa715c54a
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2023-09-05 09:27:27 +0545

    flask parent script created

---
+++ app.py
@@ -0,0 +1,36 @@
+
+from flask import Flask,request, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route("/") # route defination
+def welcome(): # call back function when route is called
+    names = ['kesv', 'dipesh', 'karan']
+    return render_template('home.html', names=names); #Things to show in route.
+
+@app.route("/about")
+def about():
+    return "I'm trying "
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # getting user data from submitted login form
+        username = request.form['username']
+        password = request.form['password']
+        # printing the user data
+        print("username:: ", username, "password:: ", password)
+
+        # returning post method
+        return redirect(url_for('welcome'))
+
+    return render_template('login.html')
+
+@app.route("/contact")
+def contact():
+    return "This is an contact page"
+
+
+if __name__=="__main__":
+    app.run()
